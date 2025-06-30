f6cfa157631fb0137eaa50b3ee6293f32f3c5b70
ResistorD <resistord@gmail.com>
2025-06-30 15:52:20 +0200

    Начальный коммит

---
+++ src/main/java/pro/skyjava/course2/examiner/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package pro.skyjava.course2.examiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/skyjava/course2/examiner/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skyjava.course2.examiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
